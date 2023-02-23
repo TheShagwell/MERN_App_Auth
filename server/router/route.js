53a11a83ecdacff833528fef792dc751d9f0dc9c
TheShagwell <kelvinerewharighe@gmail.com>
2023-02-23 03:01:42 +0100

    working on apis

---
+++ server/router/route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+
+
+// POST Methods
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'; // Http server connection
+import cors from 'cors'; // Practically, save against malicious attack
 import morgan from 'morgan'
+import connect from './database/conn.js';
+import router from './router/route.js';
+
 
 const app = express();
 
@@ -17,7 +20,20 @@ app.get('/', (req, res) => {
     res.status(201).json('Home GET Request');
 })
 
-// Start Server
-app.listen(port, () => {
-    console.log(`Shagwell Server started on port http://localhost:${port}`);
+
+// api routes
+app.use('/api', router);
+
+
+//  Start Server only when there is a valid connection
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Shagwell Server started on port http://localhost:${port}`);
+        })
+    } catch (error){
+        console.log('Cannot connect to the server');
+    }
+}).catch(error => {
+    console.log('Invalid database connection...');
 })
